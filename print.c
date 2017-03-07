921365c67a26ba398813bd36b21c1d793f14071a
Lorcan1 <lorcan.rooney@ucdconnect.ie>
2017-03-07 17:18:58 +0000

    Update print.c

--- print.c
+++ print.c
@@ -1,12 +1,12 @@
-void print(int p , int choice)
+void print(int playernumber , int choice)
 {
 	
    
-	for(int i=0; i<p; i++)
+	for(int i=0; i<playernumber; i++)
 	{
 		
-		int count = arrays[i].class;
-		printf("Player Name: %s \n", arrays[i].name);
+		int count = players[i].class;
+		printf("Player Name: %s \n", players[i].name);
 		printf("Player Class: ");
 		//type(count);
 		if(choice == 1)
@@ -26,12 +26,12 @@ void print(int p , int choice)
 	    printf("Elf");
 	    }
 		
-		printf("\nHealth is: %d\n", arrays[i].health);
-		printf("Dexterity is: %d\n", arrays[i].dexterity);
-		printf("Luck is : %d\n", arrays[i].luck);
-		printf("Magic Skills are: %d\n", arrays[i].magic);
-		printf("Strength is: %d\n", arrays[i].strength);
-		printf("Smartness: %d\n", arrays[i].smartness);
+		printf("\nHealth is: %d\n", players[i].health);
+		printf("Dexterity is: %d\n", players[i].dexterity);
+		printf("Luck is : %d\n", players[i].luck);
+		printf("Magic Skills are: %d\n", players[i].magic);
+		printf("Strength is: %d\n", players[i].strength);
+		printf("Smartness: %d\n", players[i].smartness);
 		printf("\n");
 	}
 
