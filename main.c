33981a82d1c7a9ad4263fd3c2c84f8fa0da6cdc6
Lorcan1 <lorcan.rooney@ucdconnect.ie>
2017-03-07 16:36:25 +0000

    Update main.c
    Added players main body

--- main.c
+++ main.c
@@ -3,8 +3,70 @@
 #include <time.h>
 #include "operations.h" // operations header
 
+struct player{
+	char name[20];
+	int lifepoints;
+	int class;
+	int smartness,strength,magic,luck,dexterity,health;
+	};
+
 int main(void)
 {
+	int p,i,j,choice;
+	printf("How many players are there?: ");  
+	scanf("%d",&p);
+	
+	int array[p];   
+	char a[p][p];
+	
+	for(i=0;i<p;i++)
+	{
+		int  dex=0, sm=0, st=0, l=0, m=0;
+		printf("Enter your name: ");  
+		scanf("%s",&arrays[i].name);
+		
+		printf("Choose your type: \n1 for Ogre \n2 for Human \n3 for Wizard \n4 for Elf\n");   
+		scanf("%d",&choice);
+		
+		switch(choice)
+			{
+			case 1:{
+				arrays[i].class = 0;     
+				break;
+			}
+			case 2:{
+				arrays[i].class = 1;
+				break;
+			}
+			case 3:{
+				arrays[i].class = 2;
+				break;
+			}
+			case 4:{
+				arrays[i].class = 3;
+				break;
+
+			}
+			default:{
+				printf("ERROR!");
+				exit(0);   
+			}
+			}
+			
+			stas( &dex, &l, &m, &st, &sm, &i); 
+			arrays[i].dexterity =dex;
+			arrays[i].luck = l;
+			arrays[i].magic =m;
+			arrays[i].strength = st;
+			arrays[i].smartness= sm;
+			arrays[i].health = 100;
+	}	
+
+	print(p ,choice);	
+	
+	
+	
+	
 	srand(time(NULL)); // seed the rand function
 
 	struct players player[6]; // struct players called player
