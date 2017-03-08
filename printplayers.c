10fc7766feb693eac79fc75ed9f2cf10f4618e73
Lorcan1 <lorcan.rooney@ucdconnect.ie>
2017-03-08 23:55:25 +0000

    Add files via upload

---
+++ printplayers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void printPlayers ( struct player player[])
+int main(void)
+
+{
+
+	printf("%s (%d %d)",player[currentPlayer].name, player[currentPlayer].type,player[currentPlayer].lifePoints);
+	
+}	
